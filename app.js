69578b4b31d2a5a9ed01cf6db163f6a255f4da6b
Alp <alpersyuleyman2002@gmail.com>
2023-04-20 13:20:12 +0200

    Redid all the redirects using expressJS (WOW!)

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express")
+const { rmSync } = require("fs")
 
 const app = express()
 
 
 app.listen("3000")
+
+app.get("/", (req, res)=> {
+    res.sendFile("./html/index.html", {root: __dirname})
+})
+
+app.get("/about", (req, res)=> {
+    res.sendFile("./html/about.html", {root: __dirname})
+})
+
+app.get("/about-me", (req, res)=> {
+    res.redirect("/about")
+})
+
+
+app.use((req, res)=> {
+    res.status(404).sendFile("./html/404.html",{ root: __dirname})
+})
