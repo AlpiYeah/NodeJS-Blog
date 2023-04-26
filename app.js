80b50535d7ebe448e12a66d5a6b282b82ff4cd49
Alp <alpersyuleyman2002@gmail.com>
2023-04-26 12:54:50 +0200

    created post handler and blog post function

--- app.js
+++ app.js
@@ -1,45 +1,64 @@
-const { error } = require("console")
-const express = require("express")
-const { rmSync } = require("fs")
-const mongoose = require("mongoose")
-const app = express()
-const db = "mongodb+srv://Alper:g4nAWgCNRuAAqFya@nodeblog.w7eaite.mongodb.net/test"
-const Blog = require("./models/blog")
+const { error } = require("console");
+const express = require("express");
+const { rmSync } = require("fs");
+const mongoose = require("mongoose");
+const app = express();
+const db =
+  "mongodb+srv://Alper:g4nAWgCNRuAAqFya@nodeblog.w7eaite.mongodb.net/test";
+const Blog = require("./models/blog");
 
-mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true}).then((result)=> app.listen("3000"), console.log("Successfully connected")).catch((error)=>console.log(error));
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen("3000"), console.log("Successfully connected"))
+  .catch((error) => console.log(error));
 
-app.set("view engine", "ejs")
-app.set("views", "html")
+app.set("view engine", "ejs");
+app.set("views", "html");
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
 
-app.use(express.static("public"))
-
-app.get("/", (req, res)=> {
-    res.redirect("/blogs")
-})
-
-app.get("/blogs", (req, res)=> {
-    Blog.find().sort({ createdAt: -1 })
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
     .then((result) => {
-        res.render("index", {title: "All Blogs", blogs: result})
-    }) .catch((error) => {console.log(error)
-  })
-})
+      res.render("index", { blogs: result, title: "All blogs" });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-app.get("/about", (req, res)=> {
-    res.render("about", {title: "About"})
-})
+app.post("/blogs", (req, res) => {
+  // console.log(req.body);
+  const blog = new Blog(req.body);
 
-app.get("/about-me", (req, res)=> {
-    res.redirect("/about")
-})
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get("/blogs/create", (req, res) => {
-    res.render("create",  {title: "Create a new Blog"})
-})
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
 
+app.get("/about-me", (req, res) => {
+  res.redirect("/about");
+});
 
-app.use((req, res)=> {
-    res.status(404).render("404",  {title: "404: Not found"})
-})
+app.get("/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404: Not found" });
+});
