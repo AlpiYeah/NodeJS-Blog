5aa889d2241d748757eddaf0192b3b76315b800e
Alp <alpersyuleyman2002@gmail.com>
2023-04-30 00:29:23 +0200

    made a dedicated routes file and folder

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose");
 const app = express();
 const db =
   "mongodb+srv://Alper:g4nAWgCNRuAAqFya@nodeblog.w7eaite.mongodb.net/test";
-const Blog = require("./models/blog");
 const { render } = require("ejs");
+const blogsRoute = require("./routes/blogsRoute");
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -23,50 +23,7 @@ app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
 
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { blogs: result, title: "All blogs" });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  // console.log(req.body);
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({
-        redirect: "/blogs",
-      });
-    })
-    .catch((err) => console.log(err));
-});
+app.use("/blogs", blogsRoute);
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
